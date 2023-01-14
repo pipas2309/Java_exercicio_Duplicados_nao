97ed4c88c9329476479a0be3e44713640efc88e0
Lucas Palharini <lucas230991@hotmail.com>
2023-01-14 01:57:47 -0300

    :sparkles: feat: fruit comparator created

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        FruitBasket fruitBasketA = new FruitBasket();
+        FruitBasket fruitBasketB = new FruitBasket();
+
+        String[] basketA = fruitBasketA.getBasket();
+        String[] basketB = fruitBasketB.getBasket();
+
+        String basketAString = Arrays.toString(basketA);
+        String basketBString = Arrays.toString(basketB);
+        
+        String repetedFruits = new String();
+
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                if(basketA[i].equals(basketB[j])) repetedFruits += (" " + basketA[i]);
+            }
+        }
+
+        System.out.println("\nA cesta de frutas A tem: " + basketAString);
+        System.out.println("\nA cesta de frutas B tem: " + basketBString);
+
+        System.out.println("\nAs frutas que se repetem, caso exista, são: \"" + repetedFruits + " \".");
+
+
     }
 }
---
+++ src/FruitBasket.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class FruitBasket {
+
+    private String[] fruits = {
+        "Maçâ", "Kiwi", "Pera",
+        "Uva", "Salada Mista", "Tangerina",
+        "Laranja", "Limão", "Bacate",
+        "Bacaxi", "Morango", "Melão",
+        "Mamão", "Mirtilo", "Franboesa",
+        "Cereja", "Banana", "Caju"
+    };
+    private String[] basket = new String[4];
+
+    //Usado para calcular o indice das frutas
+    int MIN = 0;
+    int MAX = 17;
+
+    public FruitBasket() {
+        for(int i = 0; i < 4; i++) {
+            int random = new Random().nextInt(17);
+            basket[i] = fruits[random];
+        }
+    }
+
+    public String[] getBasket() {
+        return basket;
+    }
+    
+}
